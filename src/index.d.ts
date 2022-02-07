2b332dbac9e5beb37e09d192a22a0ff6a70790dd
Snow <73443391+JustAWaifuHunter@users.noreply.github.com>
2022-02-07 19:47:31 -0300

    refactor: improve types & how the language system works
    * multiple language support using 1 file and types
    
    * fix eslint
    
    * lint everything
    
    * disabling "no-mixed-spaces-and-tabs" option from eslint
    
    * fix: tsc error
    
    Co-authored-by: davipatricio <davipatricio2006@gmail.com>

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,27 @@
-/* eslint-disable no-var */
-export { };
+import { data as descriptions_pt_BR } from './assets/languages/descriptions/pt_BR';
+import { data as descriptions_en_US } from './assets/languages/descriptions/en_US';
+import { data as categories_pt_BR } from './assets/languages/categories/pt_BR';
+import { data as categories_en_US } from './assets/languages/categories/en_US';
+import { data as commands_pt_BR } from './assets/languages/commands/pt_BR';
+import { data as commands_en_US } from './assets/languages/commands/en_US';
 
 import type { Client, Collection } from 'discord.js';
-import DescriptionLanguages from './modules/languages/DescriptionLanguages';
-import CommandLanguages from './modules/languages/CommandLanguages';
+import Languages from './modules/languages/Languages';
+
+interface LangExtender <T> {
+	pt_BR: T;
+	en_US: T;
+	t: (string: keyof T, lang: 'pt_BR' | 'en_US', ...args: Array<string>) => string;
+}
+
+export type Commands = typeof commands_pt_BR & typeof commands_en_US;
+export type Descriptions = typeof descriptions_pt_BR & typeof descriptions_en_US;
+export type Categories = typeof categories_pt_BR & typeof categories_en_US;
 
 export interface Language {
-	commands: CommandLanguages;
-	descriptions: DescriptionLanguages;
+	commands: Languages<'pt_BR', Commands> & LangExtender<Commands>;
+	descriptions: Languages<'pt_BR', Descriptions> & LangExtender<Descriptions>;
+	categories: Languages<'pt_BR', Categories> & LangExtender<Categories>;
 }
 
 export interface DenkyClient extends Client {
@@ -16,5 +30,6 @@ export interface DenkyClient extends Client {
 }
 
 declare global {
+	// eslint-disable-next-line no-var
 	var client: DenkyClient;
 }
---
+++ src/modules/languages/Languages.ts
@@ -0,0 +1,44 @@
+type ResponseType = string | ((...options: unknown[]) => string)
+
+const defaultSymbol = Symbol('default');
+
+export default class Languages<D extends string, L extends object> {
+	[x: string]: any;
+	[defaultSymbol]: D;
+
+	constructor(data?: [string, L][], defaultLang?: D) {
+		if (data && defaultLang) this.init(data, defaultLang);
+	}
+
+	init(data: [string, L][], defaultLang: D): this {
+		for (const d of data) {
+			this[d[0]] = d[1];
+		}
+
+		this[defaultSymbol] = defaultLang;
+
+		return this;
+	}
+
+	t(string: string, lang: string, ...args: any): string {
+		let res: ResponseType = '';
+		res = this[lang][string] || this[defaultSymbol][string];
+
+    	if (typeof res === 'function') {
+    		return res(...args) || `!{${lang}.${string}}!`;
+    	}
+
+    	return res || `!{${lang}.${string}}!`;
+	}
+}
+
+import type { data as descriptions_pt_BR } from '../../assets/languages/descriptions/pt_BR';
+import type { data as descriptions_en_US } from '../../assets/languages/descriptions/en_US';
+import type { data as categories_pt_BR } from '../../assets/languages/categories/pt_BR';
+import type { data as categories_en_US } from '../../assets/languages/categories/en_US';
+import type { data as commands_pt_BR } from '../../assets/languages/commands/pt_BR';
+import type { data as commands_en_US } from '../../assets/languages/commands/en_US';
+
+export type Commands = typeof commands_pt_BR & typeof commands_en_US;
+export type Descriptions = typeof descriptions_pt_BR & typeof descriptions_en_US;
+export type Categories = typeof categories_pt_BR & typeof categories_en_US;
