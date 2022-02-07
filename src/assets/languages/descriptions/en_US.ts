76bf181c798a202ec88fd5edc6f97558e06daa93
davipatricio <davipatricio2006@gmail.com>
2022-02-07 15:59:45 -0300

    feat: add command descriptions

--- src/assets/languages/descriptions/en_US.ts
+++ src/assets/languages/descriptions/en_US.ts
@@ -1,5 +1,6 @@
 const data = {
 	PING: 'Get the ping for Denky in your server',
+	'MINIGAMES QUIZ': 'Can you answer the questions?',
 };
 
 export { data };
--- src/assets/languages/descriptions/pt_BR.ts
+++ src/assets/languages/descriptions/pt_BR.ts
@@ -1,5 +1,6 @@
 const data = {
 	PING: 'Veja o ping do Denky no seu servidor',
+	'MINIGAMES QUIZ': 'Você pode responder as perguntas?',
 };
 
 export { data };
